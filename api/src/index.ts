a822328b470c7b5207a210c0155adf367c498238
Apoorv <apoorv11@live.com>
2026-07-30 00:56:05 +0530

    preparing api deployment

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,10 +1,21 @@
 import dotenv from "dotenv";
+import cors from "cors";
 import app from "./app";
 
 dotenv.config();
 
 const PORT = process.env.PORT
 
+app.use(
+  cors({
+    origin: [
+      "http://localhost:3000",
+      "https://your-vercel-app.vercel.app",
+    ],
+    credentials: true,
+  })
+);
+
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Server running on PORT :${PORT}`);
 });
