6b06af077d20fe468afb0b1f086ecc61dec9c85e
Apoorv <apoorv11@live.com>
2026-08-01 20:10:17 +0530

    cors fix in api

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,8 +1,18 @@
 import express from "express";
 import datasetRoutes from "./routes/dataset.routes";
+import cors from "cors";
 
 const app = express();
 
+app.use(
+  cors({
+    origin: [
+      "http://localhost:3000",
+      "https://your-vercel-app.vercel.app",
+    ],
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 app.use("/datasets", datasetRoutes);
--- api/src/index.ts
+++ api/src/index.ts
@@ -1,20 +1,9 @@
 import dotenv from "dotenv";
-import cors from "cors";
 import app from "./app";
 
 dotenv.config();
 
 const PORT = process.env.PORT
-
-app.use(
-  cors({
-    origin: [
-      "http://localhost:3000",
-      "https://your-vercel-app.vercel.app",
-    ],
-    credentials: true,
-  })
-);
 
 app.listen(PORT, () => {
   console.log(`Server running on PORT :${PORT}`);
